d161f9ac67c3122ddd2079762bbd8b32db407b73
Christian Gajo <christian_gajo@yahoo.com>
2018-08-27 08:09:51 +0800

    Added Modal Confirmation

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
         $("#accountNumberFormGroup").show();
     }
 
+    $("#transferBtn").text("Deposit");
+    $("#modalBodyText").text("Are you sure you want to make the deposit?");
+    
+
     $("#transactionType").on("change",
         function () {
             if (this.value == 0 || this.value == 1) {
@@ -14,6 +18,29 @@ $(document).ready(function () {
             } else {
                 $("#accountNumberFormGroup").show();
             }
+
+            if (this.value == 0) {
+                $("#transferBtn").text("Deposit");
+                $("#modalBodyText").text("Are you sure you want to make the deposit?");
+            } else if (this.value == 1) {
+                $("#transferBtn").text("Withdraw");
+                $("#modalBodyText").text("Are you sure you want to make the withdraw?");
+            } else {
+                $("#transferBtn").text("Transfer");
+                
+               
+            }
+            
+        });
+
+    $("#transferBtn").on("click",
+        function () {
+            
+            if ($("#transactionType").val() == 2) {
+                var accountNumberReceiver = $("#accountNumberInput").val();
+                $("#modalBodyText").text("Are you sure you want to transfer funds to account number " + accountNumberReceiver);
+            }
+            
         });
 
 });
